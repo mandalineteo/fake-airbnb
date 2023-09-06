7ac389ce6bbe089c89e2c1921d4e57c1b341057a
catherine lim <jkvvhappy@gmail.com>
2023-09-06 16:20:52 +0800

    updated devise setup except user attributes

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,9 @@
 class ListingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
   def index
+  end
+
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'listings/index'
+  get "listings/:id", to: "listings#show"
   # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
