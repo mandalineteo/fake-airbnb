2e1e3a84df7d0f0680b8c914380240f9c0abaf7e
Hedyla Yap <hedyyla@gmail.com>
2023-09-07 15:38:22 +0800

    create and new listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,6 @@
 class ListingsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_host, only: %i[new create]
 
   def index
   end
@@ -12,5 +13,22 @@ class ListingsController < ApplicationController
   end
 
   def create
+    @listing = Listing.new(listing_params)
+    @listing.host = @host
+    if @listing.save
+      redirect_to new_listing_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_host
+    @host = current_user
+  end
+
+  def listing_params
+    params.require(:listing).permit(:name, :details, :location, :max_guests, :price_per_night)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'listings/index'
-  get "listings/:id", to: "listings#show"
   # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
