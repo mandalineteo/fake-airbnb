1fbbfce515cc7da9ccb2d74189f6fd94c9b2fd79
Hedyla Yap <hedyyla@gmail.com>
2023-09-07 14:24:00 +0800

    associatedBookingsWithGuestAndListingsWithHosts

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,3 @@
 class Listing < ApplicationRecord
-  belongs_to :host, class_name: 'User', foreign_key: true
+  belongs_to :host, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :listings
+  has_many :listings, foreign_key: 'host_id'
+  has_many :bookings, foreign_key: 'guest_id'
 end
