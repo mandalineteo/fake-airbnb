0093d838057a27bafbf1447c37f2f146824b0824
Mandaline Teo <mandalineteo@hotmail.com>
2023-09-08 17:32:38 +0800

    Started coding to view all user bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,8 @@ class Booking < ApplicationRecord
   belongs_to :guest, class_name: 'User'
 
   validates :total_guests, presence: true
+
+  def main_image_key
+    listing.photos.first.key
+  end
 end
