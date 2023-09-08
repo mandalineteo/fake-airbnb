316ae2d5571837bef865867e1ade0895efbe1d8e
kathleen koh <kathleen.khy@gmail.com>
2023-09-08 17:54:18 +0800

    adding more styling to price breakdown card

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :listings, dependent: :destroy, foreign_key: 'host_id'
   has_many :bookings, dependent: :destroy, foreign_key: 'guest_id'
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}"
+  end
 end
