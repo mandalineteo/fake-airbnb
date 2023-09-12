c1089869adc00d37821ca6930617b15c99c8bb7d
kathleen koh <kathleen.khy@gmail.com>
2023-09-12 14:55:22 +0800

    building validation to prevent users from booking unavailable dates in a listing

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
     price: Number,
     cleaningFee: Number,
     serviceFee: Number,
+    unavailableDates: Array
   }
 
   static targets = [
@@ -18,25 +19,33 @@ export default class extends Controller {
     'totalNights',
     'totalNightsPrice',
     'totalPrice',
-    'freeCancellation'
+    // 'freeCancellation'
   ]
 
   connect() {
     const setDates = this.setDates.bind(this)
+    const unavailableDates = this.unavailableDatesValue
 
     this.picker = new easepick.create({
       element: this.datepickerTarget,
       css: [
         'https://cdn.jsdelivr.net/npm/@easepick/core@1.2.1/dist/index.css',
         'https://cdn.jsdelivr.net/npm/@easepick/range-plugin@1.2.1/dist/index.css',
+        'https://cdn.jsdelivr.net/npm/@easepick/lock-plugin@1.2.1/dist/index.css',
       ],
-      plugins: ['RangePlugin'],
+      plugins: ['RangePlugin', 'LockPlugin'],
       RangePlugin: {
         tooltip: true,
       },
       setup(picker) {
         picker.on("select", setDates)
-      }
+      },
+      LockPlugin: {
+        filter(fulldate) {
+          const date = `${fulldate.getFullYear()}-${('0' + (fulldate.getMonth() + 1)).slice(-2)}-${('0' + fulldate.getDate()).slice(-2)}`
+          return unavailableDates.includes(date)
+        }
+      },
     });
   }
 
@@ -72,7 +81,7 @@ export default class extends Controller {
 
 
     this.totalNightsTarget.innerText = ` X ${number_of_nights} nights`
-    this.freeCancellationTarget.innerHTML = freeCancellationHTML;
+    // this.freeCancellationTarget.innerHTML = freeCancellationHTML;
     // let price = Number(document.querySelector('[data-datepicker-price-value]').dataset.datepickerPriceValue)
     // let total_price = number_of_nights * price
     // document.querySelector("#total_nights_price").innerText = `$${total_price} SGD`
